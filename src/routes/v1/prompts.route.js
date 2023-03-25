f54b434ed41aaedc0b4724d88b5025c3d40b4859
Korhan ÖZBEK <korhanozbek44@gmail.com>
2023-03-26 01:05:17 +0300

    prompt router added.

--- src/routes/v1/prompts.route.js
+++ src/routes/v1/prompts.route.js
@@ -5,9 +5,9 @@ const promptsController = require('../../controllers/prompts.controller');
 
 const router = express.Router();
 
-// router.route('/').get(validate(promptValidation.getAll), promptsController.getAll);
+router.route('/').get(validate(promptValidation.getAll), promptsController.getAll);
 
-// router.route('/:promptId').get(validate(promptValidation.getOne), promptsController.getOne);
+router.route('/:promptId').get(validate(promptValidation.getOne), promptsController.getOne);
 router.route('/search').get(validate(promptValidation.search), promptsController.search);
 
 module.exports = router;
